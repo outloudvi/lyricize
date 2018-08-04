d11b574ace0490366d35375b21eadb9fba6c21a7
Outloudvi <olvi.git@mailnull.com>
2018-08-04 23:36:59 +0800

    Add models and apps

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(User)
+admin.site.register(Author)
+admin.site.register(Category)
+admin.site.register(Quote)
--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  uid = models.IntegerField(blank=True,default=-1)
+  desc = models.TextField(blank=True)
+  email = models.TextField(blank=True)
+  lastip = models.GenericIPAddressField()
+  def __str__(self):
+    return  str(self.uid)
+
+class Author(models.Model):
+  name = models.TextField(blank=True)
+  desc = models.TextField(blank=True)
+  def __str__(self):
+    return self.name
+
+class Category(models.Model):
+  name = models.TextField(blank=True)
+  desc = models.TextField(blank=True)
+  def __str__(self):
+    return self.name
+
+class Quote(models.Model):
+  text = models.TextField(blank=True)
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
+  author = models.ForeignKey(Author,on_delete=models.CASCADE)
+  category = models.ManyToManyField(Category)
+  def __str__(self):
+    return self.text
