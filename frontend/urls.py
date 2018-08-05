1d50a77e6f34e9bda774f572f9877ae8d573484d
Outloudvi <olvi.git@mailnull.com>
2018-08-05 21:26:07 +0800

    Add template and navbar

--- frontend/urls.py
+++ frontend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tpl/', views.tpl, name='template')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import *
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def tpl(request):
+    template = loader.get_template('tpl.html')
+    context = {
+    }
+    return HttpResponse(template.render(context, request))
