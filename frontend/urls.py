07e8e813ca7f1ddd27f43aa3216a4e4a6671f2db
Outloudvi <olvi.git@mailnull.com>
2018-08-06 23:10:18 +0800

    Add login

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('', views.mainPage, name='index'),
+    path('login/', views.showLogin, name='login'),
+    path('login.do/', views.doLogin, name='login_do'),
     path('tpl/', views.tpl, name='template'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,11 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
+from django.contrib.auth import authenticate, login
 
 from .models import *
 
 def mainPage(request):
     return render(request, 'mainPage.html', {})
+
+def showLogin(request):
+    return render(request, 'login.html', {})
+
+def doLogin(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return JsonResponse({
+            "status": "success",
+            "user": username,
+        })
+    else:
+        return JsonResponse({
+            "status": "fail",
+            "user": "",
+        })
 
 def tpl(request):
     template = loader.get_template('tpl.html')
