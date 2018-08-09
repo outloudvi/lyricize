924f841e7fba4627d74bc9e3e509c0ed75d2d20f
Outloudvi <olvi.git@mailnull.com>
2018-08-09 22:46:17 +0800

    Remove author, category & source from admin panel

--- frontend/admin.py
+++ frontend/admin.py
@@ -3,7 +3,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(User)
-admin.site.register(Author)
-admin.site.register(Category)
 admin.site.register(Lyric)
-admin.site.register(Source)
