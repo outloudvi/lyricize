9555c2d0418c1d3a7bad2d6d51c0bc981a54265f
Outloudvi <olvi.git@mailnull.com>
2018-08-09 22:45:06 +0800

    Simplify model

--- frontend/models.py
+++ frontend/models.py
@@ -8,29 +8,11 @@ class User(models.Model):
   def __str__(self):
     return  str(self.uid)
 
-class Author(models.Model):
-  name = models.TextField(blank=True)
-  desc = models.TextField(blank=True)
-  def __str__(self):
-    return self.name
-
-class Source(models.Model):
-  name = models.TextField(blank=True)
-  desc = models.TextField(blank=True)
-  def __str__(self):
-    return self.name
-
-class Category(models.Model):
-  name = models.TextField(blank=True)
-  desc = models.TextField(blank=True)
-  def __str__(self):
-    return self.name
-
 class Lyric(models.Model):
   text = models.TextField(blank=True)
   user = models.ForeignKey(User,on_delete=models.CASCADE)
-  author = models.ForeignKey(Author,on_delete=models.CASCADE)
-  category = models.ManyToManyField(Category)
-  source = models.ForeignKey(Source,on_delete=models.CASCADE)
+  author = models.TextField(blank=True)
+  category = models.TextField(blank=True)
+  source = models.TextField(blank=True)
   def __str__(self):
     return self.text
