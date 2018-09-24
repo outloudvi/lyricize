05ea5c68ce48d5db8700405491eff09c6ba65347
Outloudvi <olvi.git@mailnull.com>
2018-09-24 21:24:09 +0800

    Fix lyric model problem

--- frontend/models.py
+++ frontend/models.py
@@ -1,18 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User as sUser
 
 class User(models.Model):
-  uid = models.IntegerField(blank=True,default=-1)
-  desc = models.TextField(blank=True)
-  email = models.TextField(blank=True)
-  lastip = models.GenericIPAddressField()
-  def __str__(self):
-    return  str(self.uid)
+    uid = models.IntegerField(blank=True, default=-1)
+    desc = models.TextField(blank=True)
+    email = models.TextField(blank=True)
+    lastip = models.GenericIPAddressField()
+
+    def __str__(self):
+        return str(self.uid)
+
 
 class Lyric(models.Model):
-  text = models.TextField(blank=True)
-  user = models.ForeignKey(User,on_delete=models.CASCADE)
-  author = models.TextField(blank=True)
-  category = models.TextField(blank=True)
-  source = models.TextField(blank=True)
-  def __str__(self):
-    return self.text
+    text = models.TextField(blank=True)
+    user = models.ForeignKey(sUser, on_delete=models.CASCADE)
+    author = models.TextField(blank=True)
+    category = models.TextField(blank=True)
+    source = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.text
