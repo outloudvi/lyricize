2b3d6ce9197a174d147318c73e6002ca00f75b89
Outloudvi <olvi.git@mailnull.com>
2018-09-24 21:23:57 +0800

    Add lyric submission API

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('s/', views.submitLyric, name='submitLyric')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+from frontend.models import *
+
 
 def index(request):
     return HttpResponse("Hello, world. This is API.")
+
+
+@login_required
+def submitLyric(request):
+    lyric = Lyric()
+    lyric.text = request.POST['lyric']
+    lyric.author = request.POST['author']
+    lyric.source = request.POST['source']
+    lyric.category = request.POST['category']
+    lyric.user = request.user
+    try:
+        lyric.save()
+    except Exception as ext:
+        return JsonResponse({
+            "status": "fail",
+            "errorType": type(ext),
+            "errorArg": ext.args,
+            "user": "",
+        })
+    return JsonResponse({
+        "status": "success",
+        "id": lyric.id
+    })
